4e57a63a9b024928110803a0410a93ed1d42e5dd
Burak Doğan <me@burak-dogan.com>
2017-03-15 15:56:08 +0300

    Added index on queries

--- routes/ajax.js
+++ routes/ajax.js
@@ -15,13 +15,16 @@ module.exports = (function() {
   router.get('/departments', function (req, res) {
     templateStore.departments()
       .then(departments => res.json(departments))
-      .catch(err => res.status(500).send("Hata oluştu"));
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Hata oluştu")
+      });
   });
 
   // Get template list
   router.get('/template', (req, res) => {
-    templateStore.list()
-      .then(list => res.json(list.filter(l => req.user.isSuper || l.department.indexOf(req.user.department) > -1).map(l => Object.assign({id: l.id, name: l.name}))))
+    templateStore.list(['name', 'department'])
+      .then(list => res.json(list.filter((l,i) => (req.user.isSuper || l.department.indexOf(req.user.department) > -1) && list.findIndex(_l => _l.name == l.name) == i).map(l => Object.assign({id: l.id, name: l.name}))))
       .catch(err => res.status(500).json({message: err.message, stack: err.stack}));
   });
 
--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,9 @@ module.exports = (function() {
     templateStore.list()
       .then(templates => {
         templates = templates.filter(l => req.user.isSuper || l.department.indexOf(req.user.department) > -1);
-        var departmentTemplates = templateStore.departments(templates).filter(d => req.user.isSuper || req.user.department == d).map(d => {
-          return { name: d, templates: templates.filter(t => t.department && t.department.indexOf(d) > -1) };
-        });
-        var groupTemplates = templateStore.groups(templates).map(d => {
-          return { name: d, templates: templates.filter(t => t.group && t.group.indexOf(d) > -1).map(t => t.name) };
-        });
-        res.render('index', { user: req.user, templates, departments: departmentTemplates, groups: groupTemplates });
+        const departments = Array.prototype.concat.apply([], templates.map(t => t.department)).filter((d,i,l) => l.indexOf(d) == i).map(d => ({ name: d, templates: templates.filter(t => t.department && t.department.indexOf(d) > -1) }));
+        const groups = Array.prototype.concat.apply([], templates.map(t => t.group)).filter((d,i,l) => l.indexOf(d) == i).map(d => ({ name: d, templates: templates.filter(t => t.group && t.group.indexOf(d) > -1).map(t => t.name) }));
+        res.render('index', { user: req.user, departments, groups });
       })
       .catch(err => res.status(500).json({message: err.message, stack: err.stack}));
   });
